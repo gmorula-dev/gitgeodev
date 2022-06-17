66dbd97bc2c478554dc4a19841d2987a2f264d6d
Geo <gmorula.dev@gmail.com>
2022-06-17 01:06:41 -0400

    Ejercicios hasta la clase 18

--- dependency-injection/src/main/java/com/geodev/di/DependencyInjectionApplication.java
+++ dependency-injection/src/main/java/com/geodev/di/DependencyInjectionApplication.java
@@ -5,19 +5,27 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
+import com.geodev.di.profiles.EnvironmentService;
 import com.geodev.di.qualifiers.Nido;
+import com.geodev.di.scopes.EjemploScopeService;
 
 @SpringBootApplication
 public class DependencyInjectionApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(DependencyInjectionApplication.class);
 	
+	@Bean 
+	public String getApplicationName() {
+		return "Geo Rulz!";
+	}
+	
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(DependencyInjectionApplication.class, args);
 
-		Nido nido = context.getBean(Nido.class);
-		nido.imprimir();
+		String nombreAplicacion = context.getBean(String.class);
+		log.info("Nombre aplicacion {}", nombreAplicacion);
 	}
 
 }
