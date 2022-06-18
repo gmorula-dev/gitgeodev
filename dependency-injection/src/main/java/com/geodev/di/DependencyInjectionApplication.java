b00741f00333ba513adc18b085f1645249ea851c
Geo <gmorula.dev@gmail.com>
2022-06-18 19:16:51 -0400

    Curso hasta clase 24

--- dependency-injection/src/main/java/com/geodev/di/DependencyInjectionApplication.java
+++ dependency-injection/src/main/java/com/geodev/di/DependencyInjectionApplication.java
@@ -6,26 +6,31 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 
+import com.geodev.di.autowire.AreaCalculatorService;
+import com.geodev.di.lifecycle.LifeCycleBean;
 import com.geodev.di.profiles.EnvironmentService;
 import com.geodev.di.qualifiers.Nido;
 import com.geodev.di.scopes.EjemploScopeService;
+
+import ch.qos.logback.core.spi.LifeCycle;
 
 @SpringBootApplication
 public class DependencyInjectionApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(DependencyInjectionApplication.class);
-	
-	@Bean 
+
+	@Bean
 	public String getApplicationName() {
 		return "Geo Rulz!";
 	}
-	
+
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(DependencyInjectionApplication.class, args);
-
-		String nombreAplicacion = context.getBean(String.class);
-		log.info("Nombre aplicacion {}", nombreAplicacion);
+		LifeCycleBean bean = context.getBean(LifeCycleBean.class);
 	}
 
 }
